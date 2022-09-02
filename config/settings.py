e792e2debf61f10244bb3b69cf15a7a508197ea2
SALAH AD-DIN <97502581+skhayrullayev@users.noreply.github.com>
2022-09-03 00:23:15 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -88,19 +88,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': env.dj_db_url("DATABASE_URL")
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
-    }
+    'default': env.dj_db_url("DATABASE_URL")
 }
 
 
