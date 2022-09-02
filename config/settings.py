7f9f4009210455bd435e539092a3cac575f39278
SALAH AD-DIN <97502581+skhayrullayev@users.noreply.github.com>
2022-09-03 00:26:01 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'web-production-3cc8.up.railway.app']
 
 
 # Application definition
