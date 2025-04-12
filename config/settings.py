8241b6502065f3d36f37435256a71ae94552b45f
SALAH AD-DIN <skhayrullayev@gmail.com>
2025-04-12 17:37:14 +0500

    feat: implemented CSRF_TRUSTED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,5 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
+
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=['https://domain.uz'])
